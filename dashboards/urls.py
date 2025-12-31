e1a9a2a5efd26228020af731b11a62772e25b849
Devloper-JB <codewithjb@gmail.com>
2025-12-31 14:56:53 +0530

    Manage Users functionally for managers and editors

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 
 urlpatterns = [
+    
     path('',views.dashboard,name='dashboard'),
+
+    #Category
     path('categories/',views.categories,name='categories'),
     path('categories/add',views.add_categories,name='add_category'),
     path('categories/edit/<int:pk>',views.edit_categories,name='edit_category'),
@@ -14,4 +17,12 @@ urlpatterns = [
     path('posts/add',views.add_posts,name='add_posts'),
     path('posts/edit/<int:pk>',views.edit_posts,name='edit_posts'),
     path('posts/delete/<int:pk>',views.delete_posts,name='delete_posts'),  
+
+    #users
+    path('users/',views.users,name='users'),
+    path('users/add',views.add_users,name='add_users'),
+    path('users/edit/<int:pk>',views.edit_users,name='edit_users'),
+    path('users/delete/<int:pk>',views.delete_users,name='delete_users'),
+
+    
 ]
